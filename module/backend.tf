f69acb26f05f7628ff7c209f46229c8906f6ab51
pooja1099 <poojasangwan0510@gmail.com>
2023-03-15 12:27:17 +0530

    done

--- module/backend.tf
+++ module/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "pooja-test-123456"
-    key    = "pooja-backendfile.tfstate"
+    bucket = "nidhi-bucket12"
+    key    = "terraform.tfstate"
     region = "ap-south-1"
   }
 }
